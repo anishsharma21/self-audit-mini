a0cbcc37ae2b06cb03bffe8b1353b8abd4eafb4a
anishsharma21 <anisharma2103@gmail.com>
2024-01-19 10:32:14 +1100

    Added password encryption using bcrypt when loading users from JSON file

--- client/server/db.js
+++ client/server/db.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
 const path = require('path');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 function readJsonFile(filename) {
     const rawData = fs.readFileSync(path.resolve(__dirname, filename));
-    return JSON.parse(rawData);
+    const data = JSON.parse(rawData);
+  
+    if (filename === './models/users.json') {
+      data.forEach(user => {
+        user.password = bcrypt.hashSync(user.password, saltRounds);
+      });
+    }
+  
+    return data;
 }
 
 module.exports = {
